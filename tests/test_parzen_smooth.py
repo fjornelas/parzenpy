4944885593dbac10513c7821f907295126003a18
Javier Ornelas <jornela1@g.ucla.edu>
2024-07-18 19:13:17 -0700

    Update test_parzen_smooth.py

--- tests/test_parzen_smooth.py
+++ tests/test_parzen_smooth.py
@@ -8,16 +8,16 @@ def sample_data():
     fft = np.abs(np.fft.fft(np.sin(freq)))  # Sample FFT amplitude data
     return freq, fft
 
-def test_parzen_smooth_windowed(sample_data):
-    freq, fft = sample_data
-    fc = np.linspace(1, 9, 5)  # Sample resampled frequencies
-    b = 1.5
-    windowed_flag = True
+# def test_parzen_smooth_windowed(sample_data):
+#     freq, fft = sample_data
+#     fc = np.linspace(1, 9, 5)  # Sample resampled frequencies
+#     b = 1.5
+#     windowed_flag = True
     
-    smoothed_fas = parzenpy.parzen_smooth(freq, fft, fc, b=b, windowed_flag=windowed_flag)
+#     smoothed_fas = parzenpy.parzen_smooth(freq, fft, fc, b=b, windowed_flag=windowed_flag)
     
-    assert len(smoothed_fas) == len(fc)
-    assert np.all(np.isfinite(smoothed_fas))
+#     assert len(smoothed_fas) == len(fc)
+#     assert np.all(np.isfinite(smoothed_fas))
 
 def test_parzen_smooth_non_windowed(sample_data):
     freq, fft = sample_data
@@ -30,17 +30,17 @@ def test_parzen_smooth_non_windowed(sample_data):
     assert len(smoothed_fas) == len(fc)
     assert np.all(np.isfinite(smoothed_fas))
 
-def test_apply_smooth(sample_data):
-    freq, fft = sample_data
-    fc = np.linspace(1, 9, 5)  # Sample resampled frequencies
-    b = 1.5
-    windowed_flag = True
+# def test_apply_smooth(sample_data):
+#     freq, fft = sample_data
+#     fc = np.linspace(1, 9, 5)  # Sample resampled frequencies
+#     b = 1.5
+#     windowed_flag = True
     
-    instance = parzenpy(freq, fft)
-    smoothed_fas = instance.apply_smooth(fc, b=b, windowed_flag=windowed_flag)
+#     instance = parzenpy(freq, fft)
+#     smoothed_fas = instance.apply_smooth(fc, b=b, windowed_flag=windowed_flag)
     
-    assert len(smoothed_fas) == len(fc)
-    assert np.all(np.isfinite(smoothed_fas))
+#     assert len(smoothed_fas) == len(fc)
+#     assert np.all(np.isfinite(smoothed_fas))
 
 if __name__ == "__main__":
     pytest.main()
